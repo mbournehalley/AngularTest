b6abdc9e1160cc9137f7e5534d1a590a032d301f
mbournehalley <mbournehalley@gmail.com>
2019-06-27 11:48:06 -0700

    fix: added service test

---
+++ src/app/message.service.spec.ts
@@ -0,0 +1,24 @@
+import { MessageService } from "./message.service";
+
+describe("MessageService", () => {
+  let service: MessageService;
+
+  it("should have no message to start", () => {
+    service = new MessageService();
+    expect(service.messages.length).toBe(0);
+  });
+
+  it("should add a message when add is called", () => {
+    service = new MessageService();
+    service.add("message 1");
+    expect(service.messages.length).toBe(1);
+  });
+
+  it("should remove all message when clear is called", () => {
+    service = new MessageService();
+    service.add("message 1");
+
+    service.clear();
+    expect(service.messages.length).toBe(0);
+  });
+});
